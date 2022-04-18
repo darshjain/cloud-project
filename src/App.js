a700b500fef785921019e5ebf3b4cebe01aae6df
darshjain <darshj786@gmail.com>
2022-04-18 21:45:15 +0530

    FINAL

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import './App.css'
-import { API, Storage } from 'aws-amplify'
+import { API } from 'aws-amplify'
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 import { listNotes } from './graphql/queries'
 import {
@@ -17,25 +17,9 @@ function App() {
   useEffect(() => {
     fetchNotes()
   }, [])
-  async function onChange(e) {
-    if (!e.target.files[0]) return
-    const file = e.target.files[0]
-    setFormData({ ...formData, image: file.name })
-    await Storage.put(file.name, file)
-    fetchNotes()
-  }
+
   async function fetchNotes() {
     const apiData = await API.graphql({ query: listNotes })
-    const notesFromAPI = apiData.data.listNotes.items
-    await Promise.all(
-      notesFromAPI.map(async (note) => {
-        if (note.image) {
-          const image = await Storage.get(note.image)
-          note.image = image
-        }
-        return note
-      }),
-    )
     setNotes(apiData.data.listNotes.items)
   }
 
@@ -45,10 +29,6 @@ function App() {
       query: createNoteMutation,
       variables: { input: formData },
     })
-    if (formData.image) {
-      const image = await Storage.get(formData.image)
-      formData.image = image
-    }
     setNotes([...notes, formData])
     setFormData(initialFormState)
   }
@@ -77,7 +57,6 @@ function App() {
         placeholder="Note description"
         value={formData.description}
       />
-      <input type="file" onChange={onChange} />
       <button onClick={createNote}>Create Note</button>
       <div style={{ marginBottom: 30 }}>
         {notes.map((note) => (
@@ -85,7 +64,6 @@ function App() {
             <h2>{note.name}</h2>
             <p>{note.description}</p>
             <button onClick={() => deleteNote(note)}>Delete note</button>
-            {note.image && <img src={note.image} style={{ width: 400 }} />}
           </div>
         ))}
       </div>
